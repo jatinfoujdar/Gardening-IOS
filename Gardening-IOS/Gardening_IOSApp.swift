eb8e1cb9da2bb1fa887bf2d30278a97150880cce
Jatin Foujdar <jatin321001@gmail.com>
2025-04-13 21:46:08 +0530

    Initial Commit

---
+++ Gardening-IOS/Gardening_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gardening_IOSApp.swift
+//  Gardening-IOS
+//
+//  Created by jatin foujdar on 13/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Gardening_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
